d1158a05e6716cdea4ee18f56cb83c874d766b18
foap <foap@BI101-11.epsevg.upc.es>
2019-04-29 10:30:40 +0200

    cambios 29 de abril

---
+++ Ejercicio6funciones.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Función que dado un string:
+-devuelve la letra que más aparece
+-devuelve la letra que menos aparece */
+
+function letralaquemasaparece($cadena)
+
+
+
+$cadena=hooolaaa;
+
+?>
---
+++ Ejercico7fecha.php
@@ -0,0 +1,52 @@
+<?php
+/*funcion para fecha, tenemos dos fechas allar cual es el mayor de la fecha*/
+
+    
+   
+
+    $fecha1= strtotime("5 May 2011");
+    
+    $fecha2= strtotime("6 January 2010");
+
+    //$fecha= date ("j/n/Y H:i", $fecha1);
+    //echo "<br>primera fecha " .$fecha1;
+    //echo "<br>segunda fecha " .$fecha2;
+
+    function comparafechamayor($fecha1,$fecha2){ 
+    
+        if ($fecha1 > $fecha2) {
+            $comparacion ="la fecha1 es mayor que la fecha2";
+        
+        }elseif ($fecha1 < $fecha2 ) {
+            $comparacion ="La fecha1 es menor que la fecha2";
+        } else{
+                $comparacion = "La fecha1 es igual a fecha2";
+        }
+            return $comparacion;
+    }
+
+    /* hemos extraido esta llamada a otro php
+
+        $fecha1= strtotime("5 May 2011");
+        $fecha2= strtotime("6 January 2010");
+
+        $comparacion = comparafechamayor($fecha1,$fecha2);
+        print_r($comparacion);
+
+    
+       
+  
+           
+
+
+
+
+
+
+
+
+
+
+
+
+?>
