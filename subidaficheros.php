9a53e1f89f1820457a506140d94461d237abbef3
foap <foap@BI101-11.epsevg.upc.es>
2019-05-03 13:59:51 +0200

    cambios

---
+++ subidaficheros.php
@@ -0,0 +1,36 @@
+
+<!--comprobar en casa -->
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <!--importante no olvidarse del enctype sea "multipart/form-data" -->
+    <form action="" method="post" enctype="multipart/form-data">
+    
+    <input type="file" name="fichero"><br>
+    <input type="submit" name="submit" value="Enviar">
+    
+    </form>
+</body>
+</html>
+
+<?php
+}
+?>
