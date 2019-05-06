c155d93d51dba8116f51ef22783748b10de4e6ea
foap <foap@BI101-11.epsevg.upc.es>
2019-05-06 10:28:08 +0200

    cambios

--- subidaficheros.php
+++ subidaficheros.php
@@ -1,15 +1,25 @@
-
-<!--comprobar en casa -->
-
-
-
-
-
-
-
-
-
-
+<?php
+if(isset($_REQUEST["submit"])){
+    //print_r($_FILES); esto muestra el array del fichero
+    echo "<br><br>";
+    if(!is_uploaded_file($_FILES['fichero']['tmp_name'])){
+        echo "otro error";
+    }
+    // la camparte donde se recoge el fichero de la carpeta img
+    $dir_subida = 'img/';
+    //  se le dice la hora para subir el fichero, name el nombre fichero
+    $fichero_subido = $dir_subida . time()."_".basename($_FILES['fichero']['name']);
+   
+     if (move_uploaded_file($_FILES['fichero']['tmp_name'], $fichero_subido)) {
+        echo "<br><br>";
+        echo "El fichero es válido y se subió con éxito.\n";
+        echo "<a href=\"$fichero_subido\">imagen</a>";
+        echo "<img src=\"$fichero_subido\">";
+    } else {
+        echo "¡error!\n";
+    }
+}else{
+?>
 
 
 <!DOCTYPE html>
@@ -21,7 +31,7 @@
     <title>Document</title>
 </head>
 <body>
-    <!--importante no olvidarse del enctype sea "multipart/form-data" -->
+<!--importante no olvidarse de enctype=multipart/form-data para enviar fichero   --->
     <form action="" method="post" enctype="multipart/form-data">
     
     <input type="file" name="fichero"><br>
